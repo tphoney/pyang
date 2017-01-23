51bab1c092a6443f2769e7c6047d97297fad8f34
Rick Sherman <rick@shermdog.com>
2017-01-23 15:35:52 -0600

    Add namespaces for all modules, not just top level The default behavior of the original plugin was to only utilize the top level module namespaces.  This behavior was incorrect. There is an opportunity to refactor code later to have a single dict comprehension as we're duplicating efforts to some degree, but that would require more refactoring.  This should eventually be done, as well as porting fix to original plugin upstream.

--- pyang/plugins/allpuppet.py
+++ pyang/plugins/allpuppet.py
@@ -126,10 +126,9 @@ class AllPuppetPlugin(plugin.PyangPlugin):
             "rpc": self.ignore,
             "notification": self.ignore
         }
-        self.ns_uri = {}
-        for yam in modules:
-            self.ns_uri[yam] = yam.search_one("namespace").arg
-        # Build dictionary of all namespaces
+        # Find the namespaces for all modules, not just top level
+        self.ns_uri = {statement: statement.search_one("namespace").arg for statement, prefix in unique_prefixes(ctx).iteritems()}
+        # Build dictionary of all namespaces by prefix
         self.nsmap = {unique_prefixes(ctx)[k]: v for k, v in self.ns_uri.iteritems()}
         # nsmap[None] = "urn:ietf:params:xml:ns:netconf:base:1.0"
         self.top = ET.Element(self.doctype,
