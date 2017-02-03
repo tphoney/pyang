fd6d9b3cee6e7aaad341fcf5496f0fdd0358bba2
tphoney <tp@puppet.com>
2017-02-03 16:10:12 +0000

    remove un-needed flags

--- pyang/plugins/allpuppet.py
+++ pyang/plugins/allpuppet.py
@@ -60,11 +60,7 @@ class AllPuppetPlugin(plugin.PyangPlugin):
             optparse.make_option("--allpuppet-output-format",
                                  dest="output_format",
                                  default="all",
-                                 help="one of type/self_instances/flush/test/all"),
-            optparse.make_option("--allpuppet-naming-seed",
-                                 dest="naming_seed",
-                                 default="",
-                                 help="what is the starting point for the attribute name"),
+                                 help="one of type/flush/all"),
             optparse.make_option("--allpuppet-defaults",
                                  action="store_true",
                                  dest="sample_defaults",
@@ -122,7 +118,6 @@ class AllPuppetPlugin(plugin.PyangPlugin):
             raise error.EmitError("Unsupported document type: %s" %
                                   self.doctype)
         self.output_format = ctx.opts.output_format
-        self.naming_seed = ctx.opts.naming_seed
         self.annots = ctx.opts.sample_annots
         self.defaults = ctx.opts.sample_defaults
         self.fd = fd
@@ -259,71 +254,19 @@ class AllPuppetPlugin(plugin.PyangPlugin):
 
     def leaf(self, node, elem, module, path, type_writer=None):
         """Create a sample leaf element."""
-        if self.naming_seed == '':
-            attribute_name = node.arg
-            try:
-                description = node.search_one('description').arg
-            except:
-                description = ''
+        attribute_name = node.arg
+        try:
+            description = node.search_one('description').arg
+        except:
+            description = ''
 
-            if node.search_one('type').arg.lower() == 'empty':
-                if self.output_format in ("self_instances", "all"):
-                    xpath = "!(variable.xpath(\"{0}/{1}\").empty?) ? true : nil".format(self.tree.getpath(elem), node.arg)
-                if self.output_format in ("type", "all"):
-                    self.puppet_type(node, description, ptype='boolean', type_writer=type_writer)
-            else:
-                if self.output_format in ("self_instances", "all"):
-                    xpath = "variable.xpath(\"{0}/{1}\").text".format(self.tree.getpath(elem), node.arg)
-                if self.output_format in ("type", "all"):
-                    ptype = node.search_one('type').arg.lower() or None
-                    self.puppet_type(node, description, ptype=ptype, type_writer=type_writer)
-
+        if node.search_one('type').arg.lower() == 'empty':
+            if self.output_format in ("type", "all"):
+                self.puppet_type(node, description, ptype='boolean', type_writer=type_writer)
         else:
-            if type_writer:
-                try:
-                    prefix = elem.tag.replace('/', '_')
-                except IndexError:
-                    prefix = ''
-            else:
-                try:
-                    prefix = self.tree.getpath(elem).split(self.naming_seed)[1].replace('/', '_')
-                except IndexError:
-                    prefix = ''
-            if prefix != '':
-                #pdb.set_trace()
-                # we are nested
-                attribute_name = (prefix + "_" + node.arg)
-                # remove leading underscore
-                attribute_name = attribute_name.lstrip('_')
-            else:
-                # we are not nested
-                attribute_name = node.arg
-            try:
-                description = node.search_one('description').arg
-            except:
-                description = ''
-
             if self.output_format in ("type", "all"):
                 ptype = node.search_one('type').arg.lower() or None
                 self.puppet_type(node, description, ptype=ptype, type_writer=type_writer)
-
-            if self.output_format in ("self_instances", "all"):
-                try:
-                    actual_xpath = self.tree.getpath(elem).split(self.naming_seed)[1]
-                except IndexError:
-                    actual_xpath = ''
-
-                if actual_xpath != '':
-                    actual_xpath = "./" + self.tree.getpath(elem).split(self.naming_seed)[1] + "/" + node.arg
-                else:
-                    actual_xpath = node.arg
-
-                if node.search_one('type').arg.lower() == 'boolean':
-                    xpath = "variable.xpath(\"{0}\").text.downcase == 'true' ? true : nil".format(actual_xpath)
-                elif node.search_one('type').arg.lower() == 'empty':
-                    xpath = "!(variable.xpath(\"{0}\").empty?) ? true : nil".format(actual_xpath)
-                else:
-                    xpath = "variable.xpath(\"{0}\").text".format(actual_xpath)
 
         if self.output_format in ("flush", "all"):
             # We first need to work with the node itself, we do not know the namespace yet
@@ -360,13 +303,6 @@ class AllPuppetPlugin(plugin.PyangPlugin):
                     ns_object = dictsearch(parent.attrib['xmlns'], self.ns_uri)
                     # Add parent node and namespace to xpath
                     name_spaced_path = unique_prefixes(node.main_module().i_ctx)[ns_object] + ':' + parent.tag + '/' + name_spaced_path
-
-            # Write out the flush xpaths
-            self.puppet_flush(attribute_name, name_spaced_path)
-
-        if self.output_format in ("self_instances", "all"):
-            # Write the instance
-            self.puppet_instance(attribute_name, xpath)
 
         if node.i_default is None:
             nel, newm, path = self.sample_element(node, elem, module, path)
@@ -531,11 +467,3 @@ class AllPuppetPlugin(plugin.PyangPlugin):
             self.fd.write("""  newproperty(:{0}) do
         desc '{1}'
       end\n""".format(pname, description))
-
-    def puppet_instance(self, pname, xpath):
-        provider = ":{0}  => ".format(pname.replace('-', '_'))
-        self.fd.write(provider + xpath + ",\n")
-
-    def puppet_flush(self, pname, xpath):
-        pname = pname.replace('-', '_')
-        self.fd.write("\"" + pname + "\" => \"" + xpath + "\",\n")
